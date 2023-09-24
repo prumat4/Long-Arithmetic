3c20b5a04f5100b8179b55df651da75bddc02982
maksymdidukh <maxym.didukh@logivations.com>
2023-09-24 13:59:27 +0200

    overload operator +

--- include/LongArithmetic.hpp
+++ include/LongArithmetic.hpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <math.h>
 
-#define ARRAY_SIZE 32
+#define ARRAY_SIZE 64
 
 // num lenght is fixed 2048 bits
 // sign is separate field
@@ -14,17 +14,19 @@
 class LongNumber {
 private:
     /// 64*32 = 2048 btis 
-    std::array<uint64_t, ARRAY_SIZE> data;
+    std::array<uint32_t, ARRAY_SIZE> data;
 
 public:
     friend std::ostream& operator << (std::ostream& os, const LongNumber& ln);
     
     LongNumber();
-    LongNumber(std::string some_number);
-    LongNumber(int some_int);
-    LongNumber(std::array<uint64_t, ARRAY_SIZE> arr);
+    // LongNumber(std::string some_number);
+    // LongNumber(int some_int);
+    LongNumber(std::array<uint32_t, ARRAY_SIZE> arr);
     LongNumber(const LongNumber& other);
 
     LongNumber& operator = (const LongNumber& other);
-    LongNumber operator + (const LongNumber& other);
+    LongNumber& operator + (const LongNumber& other);
+    LongNumber& operator * (const LongNumber& other);
+
 };
--- src/LongArithmetic.cpp
+++ src/LongArithmetic.cpp
@@ -1,52 +1,42 @@
 #include "LongArithmetic.hpp"
 
-// temporary should be only line with fill(0)
 LongNumber::LongNumber() {
     data.fill(0);
 }
 
-LongNumber::LongNumber(std::array<uint64_t, ARRAY_SIZE> arr) : data(arr) {}
+LongNumber::LongNumber(std::array<uint32_t, ARRAY_SIZE> arr) : data(arr) {}
 
 LongNumber::LongNumber(const LongNumber& other) : data(other.data) {}
 
-LongNumber::LongNumber(int some_int) {
-    data.at(0) = some_int;
-}
-
-LongNumber LongNumber::operator+(const LongNumber& other) {
-    uint64_t carry = 0;
-    LongNumber answer(0);
-
-    for(int i = 0; i < ARRAY_SIZE - 1; i++) {
-        uint64_t  temp = data.at(i) + other.data.at(i) + carry;
-        answer.data.at(i) = temp & uint64_t(pow(2, 64) - 1);
-        carry = temp >> 64;
+std::ostream& operator << (std::ostream& os, const LongNumber& ln) {
+    // for(const auto& element : ln.data) {
+    //     for (int i = 31; i >= 0; i--) {
+    //         os << ((element >> i) & 1);
+    //     }
+    // }
+    for(int i = 0; i < ln.data.size(); i++) {
+        os << i << ": ";
+        os << (ln.data.at(i)) << std::endl;
     }
 
-    std::cout << answer;
-
-    return *this;
+    return os;
 }
 
-LongNumber& LongNumber::operator=(const LongNumber &other){
+
+LongNumber& LongNumber::operator = (const LongNumber &other){
     data = other.data;
 
     return *this;
 }
 
-std::ostream& operator<<(std::ostream& os, const LongNumber& ln) {
-    // for(const auto& element : ln.data) {
-        // for (int i = 63; i >= 0; i--) {
-        //     os << ((element >> i) & 1);
-        // }
-            // os << " ";
-        // os << element;
-    // }
-    for(int i = ARRAY_SIZE - 1; i >= 0; i--) {
-        // os << ln.data.at(i) << " ";
-        os << ln.data.at(i);
-    
+LongNumber& LongNumber::operator + (const LongNumber& other) {
+    uint32_t carry = 0;
+
+    for(int i = 0; i < data.size(); i++) {
+        uint64_t temp = data.at(i) + other.data.at(i) + carry;
+        data.at(i) = temp & uint64_t(pow(2, 32) - 1);
+        carry = temp >> 32;
     }
-    
-    return os;
+
+    return *this;
 }
