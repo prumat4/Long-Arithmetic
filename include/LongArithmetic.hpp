ea70fc714c62130d6808080b674b044143903d6b
maksymdidukh <maxym.didukh@logivations.com>
2023-10-07 22:51:21 +0200

    add correct version of + operation and to binary convertor

--- include/LongArithmetic.hpp
+++ include/LongArithmetic.hpp
@@ -6,16 +6,14 @@
 #include <math.h>
 
 #define ARRAY_SIZE 64
-
 // num lenght is fixed 2048 bits
-// sign is separate field
 // little endian (LSB is first)
 
 class LongNumber {
 private:
     /// 64*32 = 2048 btis 
     std::array<uint32_t, ARRAY_SIZE> data;
-
+    // uint32_t multiplyOneDigit(const LongNumber& num, const uint32_t& digit, uint32_t& answer);
 public:
     friend std::ostream& operator << (std::ostream& os, const LongNumber& ln);
     
@@ -27,12 +25,16 @@ public:
 
     LongNumber& operator = (const LongNumber& other);
     LongNumber operator + (const LongNumber& other);
-    LongNumber operator * (const LongNumber& other);
+    // LongNumber operator * (const LongNumber& other);
     LongNumber operator - (const LongNumber& other);
 
     bool operator == (const LongNumber& other);
     bool operator != (const LongNumber& other);
     bool operator > (const LongNumber& other);
     bool operator < (const LongNumber& other);
+
+
+    // convertors 
+    std::string toBinaryString() const;
 
 };
--- src/LongArithmetic.cpp
+++ src/LongArithmetic.cpp
@@ -4,9 +4,36 @@ LongNumber::LongNumber() {
     data.fill(0);
 }
 
+std::string LongNumber::toBinaryString() const {
+    std::string binaryString;
+    
+    for (int i = data.size() - 1; i >= 0; i--) {
+        uint32_t value = data[i];
+        
+        
+        for (int j = 31; j >= 0; j--) {
+            binaryString += (value & (1 << j)) ? '1' : '0';
+        }
+    }
+    
+    return binaryString;
+}
+
 LongNumber::LongNumber(std::array<uint32_t, ARRAY_SIZE> arr) : data(arr) {}
 
 LongNumber::LongNumber(const LongNumber& other) : data(other.data) {}
+
+// uint32_t LongNumber::multiplyOneDigit(const LongNumber& num, const uint32_t& digit, uint32_t& answer) {
+//     uint32_t carry = 0;
+
+//     for(int i = 0; i < num.data.size(); i++) {
+//         uint64_t temp = (num.data.at(i) * digit) + carry;
+//         answer.data.at(i) = temp & uint64_t(pow(2, 32) - 1);
+//         carry = temp >> 32;
+//     }
+//     // mb should ne size() - 1
+//     num.data.at(data.size()) = carry;
+// }
 
 std::ostream& operator << (std::ostream& os, const LongNumber& ln) {
     // for(const auto& element : ln.data) {
@@ -15,13 +42,12 @@ std::ostream& operator << (std::ostream& os, const LongNumber& ln) {
     //     }
     // }
     for(int i = 0; i < ln.data.size(); i++) {
-        os << i << ": ";
-        os << (ln.data.at(i)) << std::endl;
+        os << (ln.data.at(i)) << " ";
     }
+    os << std::endl;
 
     return os;
 }
-
 
 LongNumber& LongNumber::operator = (const LongNumber &other){
     data = other.data;
@@ -34,9 +60,9 @@ LongNumber LongNumber::operator + (const LongNumber& other) {
     LongNumber sum;
 
     for(int i = 0; i < data.size(); i++) {
-        uint64_t temp = data.at(i) + other.data.at(i) + carry;
-        sum.data.at(i) = temp & uint64_t(pow(2, 32) - 1);
-        carry = temp >> 32;
+        uint64_t temp = static_cast<uint64_t>(data.at(i)) + static_cast<uint64_t>(other.data.at(i)) + carry;
+        sum.data.at(i) = static_cast<uint32_t>(temp & 0xFFFFFFFF);
+        carry = static_cast<uint32_t>(temp >> 32); 
     }
 
     return sum;
@@ -61,8 +87,17 @@ LongNumber LongNumber::operator - (const LongNumber& other) {
     return difference;
 }
 
+// LongNumber LongNumber::operator * (const LongNumber& other) {
+//     LongNumber result;
+
+//     for(int i = 0; i < data.size(); i++) {
+//         uint64_t temp = multiplyOneDigit();
+//     }
+// }
+
 bool LongNumber::operator == (const LongNumber& other) {
     int i = data.size() - 1;
+    
     while(data.at(i) == other.data.at(i))
         i--;
 
@@ -75,6 +110,7 @@ bool LongNumber::operator != (const LongNumber& other) {
 
 bool LongNumber::operator > (const LongNumber& other) {
     int i = data.size() - 1;
+    
     while(data.at(i) == other.data.at(i))
         i--;
 
@@ -84,6 +120,4 @@ bool LongNumber::operator > (const LongNumber& other) {
 bool LongNumber::operator < (const LongNumber& other) {
     return !(*this > other);
 }
-
-
 
